0ecc40691d5b29f2cf4da953981ad18f5518abab
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-09-16 13:19:06 +0700

    - comments

--- src/main/java/com/t3h/java/module3/service/MovieService.java
+++ src/main/java/com/t3h/java/module3/service/MovieService.java
@@ -3,7 +3,6 @@ package com.t3h.java.module3.service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
